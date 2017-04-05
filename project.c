7207ccea3533535ed5a46af4fc665d67b48c6fb0
Ana Margarida Oliveira Pinheiro da Silva <up201505505@B01AT0703.fe.up.pt>
2017-04-05 16:03:52 +0100

    Diogo commit

--- project.c
+++ project.c
@@ -12,18 +12,18 @@ int end = 0;
 
 void sigint_handler(int signo)
 {
-	char answer[1];
-	printf("Are you sure you want to terminate(Y/N)? ");
-	scanf("%s",answer);
-	if(*answer == 'Y' || *answer == 'y')
-            end = 1;	
+    char answer[1];
+    printf("Are you sure you want to terminate(Y/N)? ");
+    scanf("%s",answer);
+    if(*answer == 'Y' || *answer == 'y')
+        end = 1;
 }
 void find_function(int argc, char const argv[]){
     DIR *d;
     struct dirent *dir;
     pid_t pid;
     d = opendir(argv);
-    struct stat buf; 
+    struct stat buf;
     if(d == NULL){
         exit(1);
     }
@@ -36,55 +36,59 @@ void find_function(int argc, char const argv[]){
         if(lstat(name, &buf) == -1){
             fprintf(stderr, "lstat error.\n");
             exit(1);
-            
         }
-        if(S_ISDIR(buf.st_mode)){
-            if((pid = fork()) < 0){
-                fprintf(stderr, "Fork error.\n");
-                exit(2);
-            }
-            else if(pid == 0){ //filho
-                find_function(argc, dir->d_name);
-            }
-            else { //pai
-                write(STDOUT_FILENO,dir->d_name,strlen(dir->d_name));
-                write(STDOUT_FILENO,"\n",1);
-                waitpid(pid, 0, 0);   
-            }
-        }
-        else if(S_ISREG(buf.st_mode)){
+        if(S_ISREG(buf.st_mode)){
             write(STDOUT_FILENO,dir->d_name,strlen(dir->d_name));
             write(STDOUT_FILENO,"\n",1);
         }
+            else if(S_ISDIR(buf.st_mode)){
+                if((pid = fork()) < 0){
+                    fprintf(stderr, "Fork error.\n");
+                    exit(2);
+                }
+                else if(pid > 0) { //pai
+                    write(STDOUT_FILENO,dir->d_name,strlen(dir->d_name));
+                    write(STDOUT_FILENO,"\n",1);
+                    //waitpid(pid, 0, 0);
+                    wait(NULL);
+                    }
+                else if(pid == 0){ //filho
+                find_function(argc, dir->d_name);
+                    }
+        }
     }
     end = 1;
+    
+    if (dir == NULL) {
+        exit(0);
+    }
 }
 int main(int argc, char const *argv[])
 {
-	if(argc == 0){
-		fprintf(stderr,"Invalid number of parameters.\n");
-		exit(1);
-	}
-	struct sigaction action;
-	action.sa_handler = sigint_handler;
-	sigemptyset(&action.sa_mask);
-	action.sa_flags = 0;
-
-	if(sigaction(SIGINT,&action,NULL) < 0)
-	{
-		fprintf(stderr,"Unable to install SIGINT handler\n");
-		exit(1);
-	}
-	while(end != 1){ //while there hasn't been an interruption
-		if(strcmp(argv[1],"sfind") == 0){
-			find_function(argc, argv[2]);
-		}
-		else {
-			fprintf(stderr,"Function not valid.\n");
-			exit(1);
-		}
-		if(end == 1)
-			break;
-	}
-	return 0;
+    if(argc == 0){
+        fprintf(stderr,"Invalid number of parameters.\n");
+        exit(1);
+    }
+    struct sigaction action;
+    action.sa_handler = sigint_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    
+    if(sigaction(SIGINT,&action,NULL) < 0)
+    {
+        fprintf(stderr,"Unable to install SIGINT handler\n");
+        exit(1);
+    }
+    while(end != 1){ //while there hasn't been an interruption
+        if(strcmp(argv[1],"sfind") == 0){
+            find_function(argc, argv[2]);
+        }
+        else {
+            fprintf(stderr,"Function not valid.\n");
+            exit(1);
+        }
+        if(end == 1)
+            break;
+    }
+    return 0;
 }
