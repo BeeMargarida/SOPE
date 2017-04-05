1d12d6d87e4e4e576650abfcb1ad7ed8151fc1e7
Ana Margarida Oliveira Pinheiro da Silva <up201505505@B01AT0703.fe.up.pt>
2017-04-05 17:04:38 +0100

    oki

--- project.c
+++ project.c
@@ -18,7 +18,7 @@ void sigint_handler(int signo)
     if(*answer == 'Y' || *answer == 'y')
         end = 1;
 }
-void find_function(int argc, char const argv[]){
+void find_function(char const argv[]){
     DIR *d;
     struct dirent *dir;
     pid_t pid;
@@ -41,28 +41,73 @@ void find_function(int argc, char const argv[]){
             write(STDOUT_FILENO,dir->d_name,strlen(dir->d_name));
             write(STDOUT_FILENO,"\n",1);
         }
-            else if(S_ISDIR(buf.st_mode)){
-                if((pid = fork()) < 0){
-                    fprintf(stderr, "Fork error.\n");
-                    exit(2);
-                }
-                else if(pid > 0) { //pai
-                    write(STDOUT_FILENO,dir->d_name,strlen(dir->d_name));
-                    write(STDOUT_FILENO,"\n",1);
-                    //waitpid(pid, 0, 0);
-                    wait(NULL);
-                    }
-                else if(pid == 0){ //filho
-                find_function(argc, dir->d_name);
-                    }
+        else if(S_ISDIR(buf.st_mode)){
+            if((pid = fork()) < 0){
+                fprintf(stderr, "Fork error.\n");
+                exit(2);
+            }
+            else if(pid > 0) { //pai
+                write(STDOUT_FILENO,dir->d_name,strlen(dir->d_name));
+                write(STDOUT_FILENO,"\n",1);
+                waitpid(pid, 0, 0);
+            }
+            else if(pid == 0){ //filho
+                find_function(dir->d_name);
+            }
         }
     }
     end = 1;
-    
     if (dir == NULL) {
         exit(0);
     }
 }
+
+void find_function_name(char const argv[], char *str) {
+    DIR *d;
+    struct dirent *dir;
+    pid_t pid;
+    d = opendir(argv);
+    struct stat buf;
+    if(d == NULL){
+        exit(1);
+    }
+    while((dir = readdir(d)) != NULL){
+        if(strcmp(dir->d_name,".") == 0 || strcmp(dir->d_name,"..") == 0){
+            continue;
+        }
+        char name[200];
+        sprintf(name, "%s/%s", argv, dir->d_name);
+        if(lstat(name, &buf) == -1){
+            fprintf(stderr, "lstat error.\n");
+            exit(1);
+        }
+        if(S_ISREG(buf.st_mode)){
+            strcat(str,"/");
+            strcat(str,dir->d_name);
+            printf("%s\n",str);
+        }
+        else if(S_ISDIR(buf.st_mode)){
+            if((pid = fork()) < 0){
+                fprintf(stderr, "Fork error.\n");
+                exit(2);
+            }
+            else if(pid > 0) { //pai
+                strcat(str,"/");
+                strcat(str,dir->d_name);
+                //printf("%s\n",str);
+                waitpid(pid, 0, 0);
+            }
+            else if(pid == 0){ //filho
+                find_function_name(dir->d_name,str);
+            }
+        }
+    }
+    end = 1;
+    if (dir == NULL) {
+        exit(0);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     if(argc == 0){
@@ -79,9 +124,39 @@ int main(int argc, char const *argv[])
         fprintf(stderr,"Unable to install SIGINT handler\n");
         exit(1);
     }
-    while(end != 1){ //while there hasn't been an interruption
+    
+    //              NÃO É BEM ASSIM, VAI TER DE SER UM CICLO...EU DEPOIS TRATO DISTO 
+    while(end != 1){ 
         if(strcmp(argv[1],"sfind") == 0){
-            find_function(argc, argv[2]);
+            if(argc == 3){
+                find_function(argv[2]);
+            }
+            else { //fazer os -name, -type.....
+                if(strcmp(argv[3],"-name") == 0){
+                    char *str = (char *)malloc(200*sizeof(char));
+                    //printf("fazer -name");
+                    find_function_name(argv[2],str);
+                }
+                else if(strcmp(argv[3],"-type") == 0){
+                    printf("fazer -type");
+                }
+                else if(strcmp(argv[3],"-perm") == 0){
+                    printf("fazer -perm");
+                }
+                else if(strcmp(argv[3],"-print") == 0){
+                    printf("fazer -print");
+                }
+                else if(strcmp(argv[3],"-delete") == 0){
+                    printf("fazer -delete");
+                }
+                else if(strcmp(argv[3],"-exec") == 0){
+                    printf("fazer -exec");
+                }
+                else {
+                    fprintf(stderr,"Function not valid.\n");
+                    exit(1);
+                }
+            }
         }
         else {
             fprintf(stderr,"Function not valid.\n");
